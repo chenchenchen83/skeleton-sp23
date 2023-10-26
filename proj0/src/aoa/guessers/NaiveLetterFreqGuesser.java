320c0fb9487baa76ba497c96c368b80829b58a37
chenchenchen <ccverde420@gmail.com>
2023-10-26 13:10:55 -0700

    implement NaiveLetterFreqGuesser

--- proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
@@ -1,8 +1,11 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class NaiveLetterFreqGuesser implements Guesser {
     private final List<String> words;
@@ -21,14 +24,49 @@ public class NaiveLetterFreqGuesser implements Guesser {
      *  This task is similar to something you did in hw0b! */
     public Map<Character, Integer> getFrequencyMap() {
         // TODO: Fill in this method.
-        return null;
+        Map<Character, Integer> result = new HashMap<>();
+        for(String element : words){
+            for(int i = 0; i < element.length(); i++){
+                char letter = element.charAt(i);
+                if(result.containsKey(letter)){
+                    int valueToUpdate = result.get(letter);
+                    result.put(letter, valueToUpdate+1);
+                }
+                else{
+                    result.put(letter, 1);
+                }
+            }
+        }
+        return result;
     }
 
     /** Returns the most common letter in WORDS that has not yet been guessed
      *  (and therefore isn't present in GUESSES). */
     public char getGuess(List<Character> guesses) {
         // TODO: Fill in this method.
-        return '?';
+        Map<Character, Integer> letterRemain = new TreeMap<>();
+
+        for (char key : getFrequencyMap().keySet()){
+            if(!guesses.contains(key)){
+                letterRemain.put(key, getFrequencyMap().get(key));
+            }
+        }
+
+        char charGuess = 'a';
+        int highestFreq = 0;
+        for(Map.Entry<Character, Integer> entry: letterRemain.entrySet()){
+            if(entry.getValue() > highestFreq){
+                highestFreq = entry.getValue();
+                charGuess = entry.getKey();
+            }
+        }
+
+        if(!letterRemain.isEmpty()){
+            return charGuess;
+        }
+        else{
+            return '?';
+        }
     }
 
     public static void main(String[] args) {
